fc11e0ce7b5ea61c833f6d45fdce9faabe8ba7e6
root <root@Omega-0DF2.lan>
2020-11-16 14:17:27 +0000

    Clase del miercoles 11 de noviembre

---
+++ variables.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+MI_SHELL="sh"
+echo "Estoy escribiendo mi primer script en $MI_SHELL"
+echo "Tambien puede reemplazar una variable con llaves en ${MI_SHELL}"
+echo "Estoy programando scripts en $(uname)"
